6c655d73d8324669ea605df3a09093291cc0aca3
ajkr <anuj.kumar@gft.com>
2019-11-28 15:50:21 +0100

    update pom.xml

--- src/main/java/com/example/demo/RestTestApiApplication.java
+++ src/main/java/com/example/demo/RestTestApiApplication.java
@@ -8,6 +8,7 @@ public class RestTestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestTestApiApplication.class, args);
+		System.out.println("Hi Test class");
 	}
 
 }
